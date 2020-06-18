8cbe72c263c847f4e7280bb56860bc2124711731
Jee Zhou <jeewhiz@Jees-iMac.local>
2020-06-18 01:47:50 -0500

    added heroku connection

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,26 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
-
+/*
 var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
   password: "somethingdumb",
   database: "burgers_db"
-});
+});*/
+
+var connection;
+if(process.env.JAWSDB_URl){
+  connection = mysql.createConnection(provess.env.JAWSDB_URl);
+}else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "somethingdumb",
+    database: "burgers_db"
+  });
+};
 
 // Make connection.
 connection.connect(function(err) {
