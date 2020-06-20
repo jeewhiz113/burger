60d087acd3e358adf0362f56731569af589beda1
Jee Zhou <jeewhiz@Jees-iMac.local>
2020-06-20 11:58:33 -0500

    fixed connections

--- config/connection.js
+++ config/connection.js
@@ -15,7 +15,6 @@ if(process.env.JAWSDB_URl){
 }else {
   connection = mysql.createConnection({
     host: "localhost",
-    port: 3306,
     user: "root",
     password: "somethingdumb",
     database: "burgers_db"
