be8f08035ffdb60327caaddd3ce36b6d016f578d
Jee Zhou <jeewhiz@Jees-iMac.local>
2020-06-20 12:06:54 -0500

    again another connection issue

--- config/connection.js
+++ config/connection.js
@@ -10,8 +10,8 @@ var connection = mysql.createConnection({
 });*/
 
 var connection;
-if(process.env.JAWSDB_URl){
-  connection = mysql.createConnection(process.env.JAWSDB_URl);
+if(process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
 }else {
   connection = mysql.createConnection({
     host: "localhost",
